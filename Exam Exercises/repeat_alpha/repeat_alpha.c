105461b6fbe262578e52ae9bcc893a1c4a3a5132
izzytoot <161877114+izzytoot@users.noreply.github.com>
2024-09-24 22:59:36 +0100

    Add files via upload

--- Exam Exercises/repeat_alpha/repeat_alpha.c
+++ Exam Exercises/repeat_alpha/repeat_alpha.c
@@ -1,36 +1,32 @@
 #include <unistd.h>
 
-int main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
-    int i;
-    int rep;
+	int	i;
+	int	rep;
 
-    i = 0;
-    if (argc != 2)
+	if (argc != 2)
     {
-        write (1, "/n", 1);
+        write(1, "\n", 1);
     }
     if (argc == 2)
-    {
-        rep = 0;
-        while (argv[1][i])
-        {
-            if (argv[1][i] >= 'a' && argv[1][i] <= 'z')
-            {
-                rep =(argv[1][i] - 'a') + 1;
-            }
-            else if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
-            {
-                rep = (argv[1][i] - 'A') + 1;
-            }
-            while (rep != 0)
-            {
-                write (1, &argv[1][i], 1);
-                rep --;
-            }
-            i++;
-        }
-        write (1, "/n", 1);
-    }
-    return (0);
+	{
+		i = 0;
+		while (argv[1][i])
+		{
+			rep = 1;
+			if (argv[1][i] >= 'a' && argv[1][i] <= 'z')
+				rep = rep + argv[1][i] - 'a';
+			else if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
+				rep = rep + argv[1][i] - 'A';
+			while (rep > 0)
+			{
+				write(1, &argv[1][i], 1);
+				rep--;
+			}
+			i++;
+		}
+        write(1, "\n", 1);
+	}
+	return (0);
 }
