6de6b7d8a2e3590211953be49757e39d91f4e4e6
izzytoot <161877114+izzytoot@users.noreply.github.com>
2024-10-01 17:21:21 +0100

    Add files via upload

--- C02/ex10/ft_strlcpy.c
+++ C02/ex10/ft_strlcpy.c
@@ -1,49 +1,37 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: icunha-t <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/15 14:06:13 by icunha-t          #+#    #+#             */
-/*   Updated: 2024/09/15 14:06:14 by icunha-t         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <unistd.h>
+#include <bsd/string.h>
+#include <string.h>
 #include <stdio.h>
 
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
 	unsigned int	i;
-	unsigned int	len;
+	unsigned int	r;
 
 	i = 0;
-	len = 0;
-	while (src[len] != '\0')
-	{
-		len++;
-	}
-	while (src[i] != '\0' && i < size - 1)
+	while (src[i] != '\0')
+		i++;
+    r = i;
+	i = 0;
+	while (src[i] != '\0' && i < (size - 1))
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	if (size > 0)
-	{
+	if (size)
 		dest[i] = '\0';
-	}
-	return (len);
+	return (r);
 }
-
-int	main(void)
+/*
+int main(void)
 {
-	char	src[] = "Hello!";
-	char	dest[] = "";
-	int	x;
-	
-	x = ft_strlcpy(dest, src, 5);
-	
-	printf("%s\n", dest);
-	printf("%i\n", x);
+    char dest[100];
+    char *src = "adeus";
+    int size;
+
+    size = 4;
+    printf ("%i\n", ft_strlcpy(dest, src, size));
+    printf("%s\n", dest);
+    return (0);
 }
+*/
